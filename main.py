09584af7dd1dcc51b8c5580b41ac7cade38d2da8
Alessio Gerace <a.gerace@inrim.it>
2021-11-17 12:07:00 +0100

    [FIX] project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from .libs.BrtotherDriver import BrotherDriver
+from libs.BrtotherDriver import BrotherDriver
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 printer = BrotherDriver()
